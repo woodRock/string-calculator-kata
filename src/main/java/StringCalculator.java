c4664fe1ff2eb7fef6bf8fee1703f80d54f5e83e
woodRock <j.r.h.wood98@gmail.com>
2020-03-27 17:10:59 +1300

    Add CI testing

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,6 +1,7 @@
-import java.util.*;
-import java.util.stream.*;
-import java.util.function.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class StringCalculator {
 
